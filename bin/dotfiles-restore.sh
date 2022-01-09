e64f757b53e905def381a640b3f7ad09ae3d4f97
Chris Smith <crsmithdev@gmail.com>
2022-01-09 14:27:34 -0800

    remove remote arg in restore script

--- bin/dotfiles-restore.sh
+++ bin/dotfiles-restore.sh
@@ -2,15 +2,10 @@
 
 set -xe
 
-# arg check
-if [[ $# -ne 1 ]]; then
-    echo "Usage: $0 <remote repository>"
-fi
-
 DF_PATH=$HOME/.dotfiles
 alias dotfiles='git --git-dir=$DF_PATH --work-tree=$HOME'
 
-git clone -b main --bare $1 $DF_PATH
+git clone -b main --bare git@github.com:crsmithdev/dotfiles $DF_PATH
         
 dotfiles config --local status.showUntrackedFiles no
 dotfiles checkout || echo "Move or delete conflicting files first, then run 'dotfiles checkout' to finish restore."
